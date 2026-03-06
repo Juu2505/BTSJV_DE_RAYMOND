fdef34d145c664b39dcac5cd314eb623c734040e
Juu2505 <julie.myriam.durand@gmail.com>
2026-03-06 16:17:43 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>IRIS</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container-fluid p-5 bg-primary text-white text-center">
+  <h1>BTS SIO 1 JV</h1>
+  <p>La meilleure promo d'IRIS, enfin presque...</p> 
+</div>
+  
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3><a href="cours/Chapitre 1 - Notions générales.pdf" target="_BLANK"><img src="images/cours.jpg" width="50"></a> Notions Générales</h3>
+      <p>Les bases de la programmation en langage C et PHP</p>
+      <p><a href="exercices/MCAmoy.png" target="_BLANK"><img src="images/exo.png" width="50"></a>Élaborer un MCA qui calcule la moyenne de trois notes.</p>
+    </div>
+    
+    <div class="col-sm-4">
+      <h3><img src="images/cours.jpg" width="50"> Les Alternatives</h3>
+      <p>Alternatives simple, alternative double, choix multiples</p>
+      <p><a href="exercices/moy.php" target="_BLANK"><img src="images/php.png" width="50"></a><a href="exercices/jourC.png" target="_BLANK"><img src="images/langagec.png" width="30"></a>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+
+    <div class="col-sm-4">
+      <h3><img src="images/cours.jpg" width="50"> Les Boucles</h3>        
+      <p>While, do...While, For</p>
+      <p><a href="exercices/moy.php" target="_BLANK"><img src="images/php.png" width="50"></a>Calculer la moyenne de trois notes.</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
